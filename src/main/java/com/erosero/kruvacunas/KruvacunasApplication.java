b1411ecf90e4856ef012729f5b30c13769d45bd9
EstebanRP98 <davidesteb98@gmail.com>
2022-09-01 22:58:32 -0500

    creacion del poryecto

---
+++ src/main/java/com/erosero/kruvacunas/KruvacunasApplication.java
@@ -0,0 +1,13 @@
+package com.erosero.kruvacunas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KruvacunasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KruvacunasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erosero/kruvacunas/KruvacunasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erosero.kruvacunas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KruvacunasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
